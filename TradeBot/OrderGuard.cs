0c93292f5bc1745fc8761761f3a2d46ec76fb0f1
Danilo Gomes <danilooliveira9@hotmail.com>
2021-10-25 22:16:38 -0300

    added namespace system for idisposable

--- TradeBot/OrderGuard.cs
+++ TradeBot/OrderGuard.cs
@@ -3,7 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using TradeBot.Responses;
-using System.Runtime;
+using System;
 
 namespace TradeBot
 {
