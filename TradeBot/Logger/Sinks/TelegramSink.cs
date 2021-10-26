b20b4069ebe18fd7ea7b6ed6c01baca9023f760f
Danilo Gomes <danilooliveira9@hotmail.com>
2021-10-25 22:18:54 -0300

    using system.threading

--- TradeBot/Logger/Sinks/TelegramSink.cs
+++ TradeBot/Logger/Sinks/TelegramSink.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using Telegram.Bot;
 using TradeBot.Logger;
 using TradeBot.Services;
